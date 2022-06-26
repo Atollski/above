0db83df2218612e7b690a24d1c50c32958dff51b
atollski <atollski@Flex>
2022-06-27 08:20:48 +1000

    Added non clipping cockpit and mouse lock

--- core.js
+++ core.js
@@ -16,23 +16,28 @@ window.addEventListener("keydown", event=>{keys[event.keyCode] = 1;});
 //window.addEventListener("keydown", event=>{console.log(event.keyCode);});
 window.addEventListener("keyup", event=>{keys[event.keyCode] = 0;});
 window.addEventListener("wheel", event=>{
-	player.impulse = player.impulse || new THREE.Vector3();
-	player.impulse.y = Math.max(player.impulse.y - event.deltaY*0.1, 0); // cannot go below 0
-	// torque impulse relative to the player orientation (mainly pitch and roll)
-	player.torqueImpulse = player.torqueImpulse || new THREE.Vector3();
-	player.torqueImpulse.z -= event.deltaX*0.5; // roll correction via mouse wheel
+	if (player) {
+		player.impulse = player.impulse || new THREE.Vector3();
+		player.impulse.y = Math.max(player.impulse.y - event.deltaY*0.1, 0); // y scrolling controls vertical impulse - cannot go below 0
+		// torque impulse relative to the player orientation (mainly pitch and roll)
+		player.torqueImpulse = player.torqueImpulse || new THREE.Vector3();
+		player.torqueImpulse.z -= event.deltaX*0.5; // roll applied via mouse wheel x scrolling
+	}
 });
 window.addEventListener("mousemove", event=>{
-	// player rotation around the Y axis (yaw) should not factor in the orientation of the player and remain in line with the horizon
-	player.horizonTorqueImpulse = player.horizonTorqueImpulse || new THREE.Vector3();
-	// torque impulse relative to the player orientation (mainly pitch and roll)
-	player.torqueImpulse = player.torqueImpulse || new THREE.Vector3();
-	
-	// just add forces in for now
-	player.horizonTorqueImpulse.y -= event.movementX * 2.0;
-	player.torqueImpulse.x += event.movementY * 2.0;
-});
+	if (player) {
+		// player rotation around the Y axis (yaw) should not factor in the orientation of the player and remain in line with the horizon
+		player.horizonTorqueImpulse = player.horizonTorqueImpulse || new THREE.Vector3();
+		// torque impulse relative to the player orientation (mainly pitch and roll)
+		player.torqueImpulse = player.torqueImpulse || new THREE.Vector3();
 
+		// just add forces in for now
+		player.horizonTorqueImpulse.y -= event.movementX * 2.0;
+		player.torqueImpulse.x += event.movementY * 2.0;
+	}
+});
+window.addEventListener('contextmenu', event => event.preventDefault()); // disable right click
+//
 // Utilities to convert from THREE to Ammo
 THREE.Vector3.prototype.ammo = function() { // Vector conversion
 	return new Ammo.btVector3(this.x,this.y,this.z);
@@ -41,6 +46,10 @@ THREE.Vector3.prototype.ammo = function() { // Vector conversion
 THREE.Quaternion.prototype.ammo = function() { // Quaternion conversion
 	return new Ammo.btQuaternion(this.x,this.y,this.z, this.w);
 };
+
+THREE.Mesh.prototype.ammo = function() { // attach physics to mesh
+	
+}
 
 function start () {
 	tmpTrans = new Ammo.btTransform();
@@ -107,6 +116,7 @@ function setupGraphics(){
 	renderer.gammaInput = true;
 	renderer.gammaOutput = true;
 	renderer.shadowMap.enabled = true;
+	renderer.domElement.onclick = () => {renderer.domElement.requestPointerLock();}	
 }
 
 function renderFrame() {
@@ -134,10 +144,8 @@ function renderFrame() {
 	if (player.torqueImpulse) {
 		// before applying torque impulse, calculate roll relative to horizon and correct for it to stabilize the aircraft
 		let euler = new THREE.Euler().setFromQuaternion(player.quaternion, "YXZ"); // euler order important here since I want to process purely the Z rotation
-		//console.log(euler.z);
-		player.torqueImpulse.z -= euler.z * 5.0; // firstly, attempt to correct any roll around Z axis
-		//console.log(euler.x);
-		player.torqueImpulse.x -= euler.x * 4 * Math.max(0,Math.abs(euler.x)-(Math.PI / 4)); // 22.5 deg dead zone
+		player.torqueImpulse.z -= euler.z * 5.0; // firstly, attempt to stabilise any roll around Z axis
+		player.torqueImpulse.x -= euler.x * 4 * Math.max(0,Math.abs(euler.x)-(Math.PI / 4)); // ±45° dead zone
 		
 		let torqueImpulse = player.torqueImpulse.clone();
 		torqueImpulse.multiplyScalar(deltaTime);
@@ -181,16 +189,28 @@ function createBlock(){
 
 function createPlayer(){
 	let pos = {x: 0, y: 20, z: 0};
-	let scale = {x: 2, y: 2, z: 2};
+	//let scale = {x: 2, y: 2, z: 2};
 	let mass = 1;
 
 	//threeJS Section
-	player = new THREE.Mesh(new THREE.BoxBufferGeometry(), new THREE.MeshPhongMaterial({color: 0xff0505}));
-	player.scale.set(scale.x, scale.y, scale.z);
+	player = new THREE.Mesh(new THREE.BoxBufferGeometry(4,2,7), new THREE.MeshPhongMaterial({color: 0xff0505}));
+	player.scale.set(1,1,1);
 	player.rotation.set(0.2,0,0.1);
 	player.position.set(pos.x, pos.y, pos.z);
 	player.castShadow = true;
 	player.receiveShadow = true;
+	
+	let cockpit = new THREE.Mesh(new THREE.BoxBufferGeometry(3,1.5,3), new THREE.MeshPhongMaterial({color: 0x0000ff}));
+	cockpit.material.transparent = true;
+	cockpit.material.opacity = 0.5;
+	cockpit.scale.set(1,1,1);
+	cockpit.position.set(
+		0//(player.geometry.parameters.width*0.5) - (cockpit.geometry.parameters.width*0.5)
+		,(player.geometry.parameters.height*0.5) - (cockpit.geometry.parameters.height*0.5) + 0.2
+		,(cockpit.geometry.parameters.depth*0.5) - (player.geometry.parameters.depth*0.5) - 1
+	);
+	player.add(cockpit);
+	
 	scene.add(player);
 
 	//Ammojs Section
@@ -200,7 +220,11 @@ function createPlayer(){
 	transform.setRotation(player.quaternion.ammo());
 	
 	let motionState = new Ammo.btDefaultMotionState(transform);
-	colShape = new Ammo.btBoxShape(new Ammo.btVector3(scale.x*0.5, scale.y*0.5, scale.z*0.5));
+	colShape = new Ammo.btBoxShape(new Ammo.btVector3(
+		player.geometry.parameters.width*0.5
+		, player.geometry.parameters.height*0.5
+		, player.geometry.parameters.depth*0.5
+	));
 	let localInertia = new Ammo.btVector3(0, 0, 0);
 	colShape.calculateLocalInertia(mass, localInertia);
 	let rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, colShape, localInertia);
